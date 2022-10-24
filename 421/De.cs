ba574328890f843880c338c5f5628ddcc2e9ce88
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-24 12:57:43 +0200

    Initialisation de la classe De

---
+++ 421/De.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _421
+{
+    class De
+    {
+        string nbFaces;
+        Random random;
+        private int face;
+        public int Face
+        {
+            get { return face; }
+            set { }
+        }
+
+        public De(string NbFaces, int aFace)
+        {
+            NbFaces = NbFaces;
+            Face = aFace;
+        }
+
+        public string ToString()
+        {
+            string toString = "";
+            return toString;
+        }
+
+        public int Lancer()
+        {
+            int lancer;
+            return lancer;
+        }
+    }
+}
