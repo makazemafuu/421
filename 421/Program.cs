a025ef6effa9ace62fb25e103a8457ab99d3012a
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-25 14:31:16 +0200

    Affichage

--- 421/Program.cs
+++ 421/Program.cs
@@ -9,27 +9,27 @@ namespace _421
         static void Main(string[] args)
         {
 
-            Console.WriteLine("421 Game\n");
+            Console.WriteLine("--- 421 Game ---\n");
 
-            Console.WriteLine("Hello there, welcome to the best game ever ! *coughs*\n");
+            Console.WriteLine("Hello there, welcome to the best game ever ! *coughs*");
             Console.WriteLine("What's your name, pal ?\n");
             string name = Console.ReadLine();
 
-            Console.WriteLine("Very well {0}. With how many dice would you like to play with ?\n", name);
+            Console.Write("\nVery well {0}. With how many dice would you like to play with ? Please choose :  ", name);
             int nbNormalDice = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Alright ! Now pssst... come closer... should I add in some loaded dice for you young fellow ? How many ?\n");
+            Console.Write("\nAlright ! Now pssst... come closer... should I add in some loaded dice for you young fellow ? How many ? Please choose : ");
             int nbLoadedDice = int.Parse(Console.ReadLine());
 
-            Console.Write("I knew you were a team player ! *cougs*\n");
-            Console.WriteLine("How many rounds will it be, sir ?\n");
+            Console.Write("\nI knew you were a team player ! *cougs*");
+            Console.Write("How many rounds will it be, sir ? Please choose : ");
             int nbRounds = int.Parse(Console.ReadLine());
 
             Game game = new Game(nbNormalDice, nbLoadedDice, nbRounds);
             game.Run();
 
             int finalPlayerScore = game.Score();
-            Console.WriteLine("Your score is of {0}\n", finalPlayerScore);
+            Console.WriteLine("\nYour score is of {0}\n", finalPlayerScore);
 
 
 
